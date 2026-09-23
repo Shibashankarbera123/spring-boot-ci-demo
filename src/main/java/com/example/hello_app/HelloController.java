f2e1f1b68364dd869fcbb1beeca138882dd83308
student shibashankar <240301120230@centurionuniv.edu.in>
2026-09-23 22:12:13 +0530

    update

--- src/main/java/com/example/hello_app/HelloController.java
+++ src/main/java/com/example/hello_app/HelloController.java
@@ -9,7 +9,7 @@ public class HelloController {
     @GetMapping("/")
 
     public String hello(){
-        return "Hello from CI/CD pipeline";
+        return "Hello from CI/CD pipeline......";
     }
 
 }
